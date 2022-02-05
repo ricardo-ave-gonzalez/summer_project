25eeafe25fa4cdcb85c67ee8bf36605214c43414
ricardo-ave-gonzalez <ricardo.ave.gonzalez@gmail.com>
2022-02-05 20:48:49 -0300

    manejamos el formData y sus values

--- cliente/summer/src/components/SignUpForm/SignUpForm.js
+++ cliente/summer/src/components/SignUpForm/SignUpForm.js
@@ -1,28 +1,57 @@
-import React from 'react'
-import { Row, Col, Form, Button, Spinner} from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Row, Col, Form, Button, Spinner } from 'react-bootstrap'
 
 import "./SignUpForm.scss"
 
 export default function SignUpForm(props) {
     const { setMostrarModal } = props
+    const [formData, setFormData] = useState(initialFormValue())
 
     const onSubmit = e => {
-        e.preventDefault();
+        e.preventDefault()
         setMostrarModal(false)
+
+        console.log(formData)
+    }
+
+    /**
+     * podemos centralizar todos en 
+     * un onChange, SOLAMENTE si TODOS son del tipo 
+     * input como en nuestro caso.
+     * @param {*} e 
+     */
+    const onChange = e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value})
     }
 
     return (
         <div className="sign-up-form">
             <h2>Crea tu cuenta</h2>
-            <Form onSubmit={onSubmit}>
+            <Form onSubmit={onSubmit} onChange={onChange}>
 
                 <Form.Group>
                     <Row>
                         <Col>
-                            <Form.Control typo="text" placeholder="Nombre"/>
+                            <Form.Control
+                                type="text"
+                                placeholder="Nombre"
+                                name="nombre"
+                                /* onChange={e => 
+                                    setFormData({ ...formData, nombre: e.target.value})
+                                } */
+                                defaultValue={formData.nombre}
+                            />
                         </Col>
                         <Col>
-                            <Form.Control typo="password" placeholder="Contraseña"/>
+                            <Form.Control
+                                type="password" 
+                                placeholder="Contraseña"
+                                name="password"
+                                /* onChange={e => 
+                                    setFormData({ ...formData, password: e.target.value})
+                                } */
+                                defaultValue={formData.password}
+                             />
                         </Col>
                     </Row>
                 </Form.Group>
@@ -30,10 +59,26 @@ export default function SignUpForm(props) {
                 <Form.Group>
                     <Row>
                         <Col>
-                            <Form.Control typo="email" placeholder="Correo Electronico"/>
+                            <Form.Control 
+                                type="email" 
+                                placeholder="Correo Electronico"
+                                name="email" 
+                                /* onChange={e => 
+                                    setFormData({ ...formData, email: e.target.value})
+                                }     */
+                                defaultValue={formData.email}
+                            />
                         </Col>
                         <Col>
-                            <Form.Control typo="password" placeholder="Repetir Contraseña"/>
+                            <Form.Control 
+                                type="password" 
+                                placeholder="Repetir Contraseña" 
+                                name="repeatPassword"
+                                /* onChange={e => 
+                                    setFormData({ ...formData, repeatPassword: e.target.value})
+                                } */
+                                defaultValue={formData.repeatPassword}
+                            />
                         </Col>
                     </Row>
                 </Form.Group>
@@ -44,4 +89,13 @@ export default function SignUpForm(props) {
             </Form>
         </div>
     );
+}
+
+function initialFormValue() {
+    return {
+        nombre: "",
+        email: "",
+        password: "",
+        repeatPassword: ""
+    }
 }
