dba540d9b3bef388e79f67645680e77d6c555127
ricardo-ave-gonzalez <ricardo.ave.gonzalez@gmail.com>
2022-04-15 13:02:00 -0300

    test layout y scroll de content

--- src/layout/BasicLayout.js
+++ src/layout/BasicLayout.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import { Container, Row, Col} from 'react-bootstrap'
 
 import "./BasicLayout.scss";
 
 export default function BasicLayout(props) {
-  const { children } = props  
-  console.log(props)  
+  const { className, children } = props  
+  //console.log(props)  
   return (
-    <div>
-        <h2>MENU</h2>
-        {children}
-    </div>
+    <Container className={`basic-layout ${className}`}>
+      <Row>
+        <Col xs={3} className="basic-layout__menu">
+          <h2>Menu...</h2>
+        </Col>
+        <Col xs={9} className="basic-layout__content">
+          {children}
+        </Col>
+      </Row>
+    </Container>
   )
 }
